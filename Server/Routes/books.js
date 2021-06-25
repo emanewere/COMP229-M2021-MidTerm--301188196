1cc8909d517bf016a362570f7a07d7adbd9e76aa
Ofovwe Ewere <ewereeman@gmail.com>
2021-06-25 18:16:15 -0500

    Remote URI for mongo atlas database added

--- Server/Routes/books.js
+++ Server/Routes/books.js
@@ -51,7 +51,31 @@ router.get('/:id', (req, res, next) => {
     });
 });
 router.post('/:id', (req, res, next) => {
+    let id = req.params.id;
+    let updatedBookItem = new books_1.default({
+        "_id": id,
+        "Title": req.body.title,
+        "Description": req.body.description,
+        "Price": req.body.price,
+        "Author": req.body.author,
+        "Genre": req.body.genre
+    });
+    books_1.default.updateOne({ _id: id }, updatedBookItem, {}, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/books');
+    });
 });
 router.get('/delete/:id', (req, res, next) => {
+    let id = req.params.id;
+    books_1.default.remove({ _id: id }, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/books');
+    });
 });
 //# sourceMappingURL=books.js.map
